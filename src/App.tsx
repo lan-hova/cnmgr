32b9c01d607fedac2465cb189cc417eae4c30335
lannn <holan2k@gmail.com>
2025-06-16 02:57:25 +0700

    done up web

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,7 +13,6 @@ import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    
     <Router>
       <ScrollToTop />
       <div className="min-h-screen bg-white">
